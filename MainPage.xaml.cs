6f8b7c8c33507ff67e4fe7c77c426fb0a5b45c3e
Ofer Zadikario <ofer.zadikario@gmail.com>
2023-12-28 13:21:09 +0200

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MauiApp2;
+
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+	List<Monkey> monkeyList;
+	public MainPage()
+	{
+		InitializeComponent();
+        monkeyList = Monkey.GetMonkeys();
+        count = 0;
+        TheImage.Source = monkeyList[count].Image;
+        TheLabel.Text = monkeyList[count].Name;
+        BtnUp.IsEnabled = true;
+    }
+
+
+    private void BtnUp_Clicked(object sender, EventArgs e)
+    {
+        
+        if (count < monkeyList.Count-1)
+        {
+            count++;
+            TheImage.Source = monkeyList[count].Image;
+            TheLabel.Text = monkeyList[count].Name;
+            BtnDown.IsEnabled = true;
+        }
+        if (count == monkeyList.Count - 1)
+        {
+            BtnUp.IsEnabled = false;
+            
+        }
+        
+    }
+
+    private void BtnDown_Clicked(object sender, EventArgs e)
+    {
+        
+        if (count > 0)
+        {
+            count--;
+            TheImage.Source = monkeyList[count].Image;
+            TheLabel.Text = monkeyList[count].Name;
+            BtnUp.IsEnabled = true;
+        }
+        if (count == 0)
+        {
+            
+            BtnDown.IsEnabled = false;
+        }
+        
+    }
+}
+
