716c8fa7bbbd5e5adaf13e49eb9a01fe9b401ef5
Ofer Zadikario <ofer.zadikario@gmail.com>
2023-12-31 12:17:12 +0200

    add content for next lesson

--- App.xaml.cs
+++ App.xaml.cs
@@ -4,6 +4,8 @@ public partial class App : Application
 {
 	public App()
 	{
+        //1. show how to add a new page and use it in the app as main page
+        //2. show how an object can be inserted dynamically to the page
 		InitializeComponent();
         //MainPage = new MainPage();
         //MainPage = new DynamicPage();
